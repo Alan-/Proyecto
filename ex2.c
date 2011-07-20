3fabfcc9b56e3b768d04f00103d7c20a697ab8a7
Alan <mudkipz9k@gmail.com>
2011-07-19 21:08:16 -0600

    there we go

---
+++ ex2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+	printf("\aHello, World\n");
+	printf("\bHello, World\n");
+	printf("\fHello, World\n");
+	printf("\nHello, World\n");
+	printf("\rHello, World\n");
+	printf("\tHello, World\n");
+	printf("\vHello, World\n");
+	printf("\\Hello, World\n");
+	printf("\?Hello, World\n");
+	printf("\'Hello, World\n");
+	printf("\"Hello, World\n");
+}
