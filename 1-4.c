d216f1ad90a392df4f4c3c7493bf8540f4aa35bc
Alan <mudkipz9k@gmail.com>
2011-07-27 17:00:14 -0600

    slowpoke.jpg

---
+++ 1-4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main()
+{
+	float fahr, celsius;
+	float lower, upper, step;
+	lower = 0;
+	upper = 100;
+	step = 10;
+	celsius = lower;
+	printf("  Celsius    Fahrenheit\n");
+	while (celsius <= upper) 
+	{
+		fahr = (celsius * (9.0/5.0)) + 32.0;
+		printf("   %3.0f      %6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
